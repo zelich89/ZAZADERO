5aa7acd8aeb3c589a9c71ee5dcc91a942b358a57
unknown <C:\Users\Zelich\AppData\Roaming\The Bat!>
2019-10-13 19:09:56 +0300

    13/10-2

--- js/left-bar.js
+++ js/left-bar.js
@@ -1,6 +1,6 @@
 $(".js-burger").click(function() {
     $(".js-sect-1").toggleClass("main-nav_open");
-    $(this).toggleClass("main-nav-open-btn_active");
+    $(this).toggleClass("js-burger_active");
     $(".js-layer").toggleClass("js-layer-active");
-    $('.main-nav').toggleClass('active');
+    $('.sect-1').toggleClass('active');
 });
