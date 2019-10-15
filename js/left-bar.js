8de8c08d66de04e5d122f48de3ac474ae0e2bd65
unknown <C:\Users\Zelich\AppData\Roaming\The Bat!>
2019-10-15 20:50:34 +0300

    Переделка меню

--- js/left-bar.js
+++ js/left-bar.js
@@ -1,9 +1,5 @@
 $(".js-switch").click(function() {
-    $(".js-main-nav").toggleClass("main-nav_open");
-    $(this).toggleClass("main-nav-open-btn_active");
-    $(".js-layer").toggleClass("js-layer-active");
-    $('.main-nav').toggleClass('active');
-    $('.main-text-dark').toggleClass('active');
+    $(".js-sect-1").toggleClass("active");
 });
 
 
